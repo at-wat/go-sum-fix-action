8ae86667ce8ab601214bd0daca9c0d3f906eb043
Atsushi Watanabe <atsushi.w@ieee.org>
2021-07-04 14:22:20 +0900

    Add test

--- test/tidied/dummy.go
+++ test/tidied/dummy.go
@@ -3,4 +3,5 @@ package dummy
 import (
 	_ "github.com/at-wat/ebml-go"
 	_ "github.com/at-wat/mqtt-go"
+	_ "github.com/google/go-github/v35/github"
 )
--- test/tidied/go.mod
+++ test/tidied/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/at-wat/ebml-go v0.0.1
 	github.com/at-wat/mqtt-go v0.11.0
+	github.com/google/go-github/v35 v35.3.0
 )
