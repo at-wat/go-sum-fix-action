e22b5ea9e46fe6822d8165f161d32c5e2f949446
Atsushi Watanabe <atsushi.w@ieee.org>
2025-11-25 10:55:16 +0900

    Debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -85,9 +85,9 @@ trap revert_git_config EXIT
 
 git config --unset-all http."https://github.com/".extraheader || true
 git config --global --unset-all http."https://github.com/".extraheader || true
-git config --get-regexp '^includeif\.gitdir:.*/git-credentials.*'
+git config --get-regexp '^includeif\.gitdir:.*/git-credentials.*' || true
 git config --get-regexp '^includeif\.gitdir:.*/git-credentials.*' \
-  | xargs -n1 git config --unset-all
+  | xargs -n1 git config --unset-all || true
 
 echo "--- cleaned git config"
 git config --list --show-origin
